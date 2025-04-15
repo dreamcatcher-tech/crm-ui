26e880c01bdb42f85f5a0bc81d8fb27837d4d102
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2025-04-15 01:57:14 +0000

    connect to server

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,35 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+import { createWebArtifact } from '@artifact/context/web-client';
+
+const url = import.meta.env.VITE_WEB_CLIENT_URL;
+const identity = import.meta.env.VITE_DID;
+
+let bearerToken = localStorage.getItem('bearerToken');
+if (!bearerToken) {
+  bearerToken = prompt('Enter authorization token');
+  if (!bearerToken) {
+    throw new Error('No authorization token provided');
+  }
+  localStorage.setItem('bearerToken', bearerToken);
+}
+
+let artifact
+try {
+  artifact = await createWebArtifact(url, identity, bearerToken);
+} catch (error) {
+  console.error(error);
+  localStorage.removeItem('bearerToken');
+  alert(error)
+  window.location.reload();
+}
+
+(globalThis as any).artifact = artifact;
+
+
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
+
+loadEnv('development', '.');
 
 // https://vitejs.dev/config/
 export default defineConfig({
